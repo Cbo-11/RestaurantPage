a1745a683317a9634ac9bb8b2cb1780685086781
Cbo-11 <c.bolland@live.com>
2021-03-10 21:00:45 +0000

    index page setup and draft html for each page

---
+++ src/menu.js
@@ -0,0 +1,18 @@
+var menuItems = {}
+
+
+<div class="container-xxl my-md-4 bd-layout">
+    <main class="bd-main order-1">  
+        <div id="menu-details" class="container-sm overflow-auto gap-3" >
+                <h2 class="title"> Menu </h2>
+                <div class="menu-item">
+                <h4 id="fat">Halibut - £18.00</h4>
+                <p>insert text </p>
+                </div>
+                <div class="menu-item">
+                <h4 id="one">Dish 2</h4>
+                <p>...</p>
+                </div>
+        </div>          
+    </main>
+</div>
